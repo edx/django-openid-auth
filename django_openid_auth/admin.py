aa195a2110dbe0eb15d7da96ead57d18775c425d
James Henstridge <james@jamesh.id.au>
2009-04-02 11:17:55 +0800

    Some tweaks to the admin interface code, including exposing cleanup actions (only available if using Django 1.1 though).

--- django_openid_auth/admin.py
+++ django_openid_auth/admin.py
@@ -1,20 +1,38 @@
 from django.contrib import admin
 from django_openid_auth.models import Nonce, Association, UserOpenID
+from django_openid_auth.store import DjangoOpenIDStore
 
 
 class NonceAdmin(admin.ModelAdmin):
     list_display = ('server_url', 'timestamp')
+    actions = ['cleanup_nonces']
+
+    def cleanup_nonces(self, request, queryset):
+        store = DjangoOpenIDStore()
+        count = store.cleanupNonces()
+        self.message_user(request, "%d expired nonces removed" % count)
+    cleanup_nonces.short_description = "Clean up expired nonces"
 
 admin.site.register(Nonce, NonceAdmin)
+
 
 class AssociationAdmin(admin.ModelAdmin):
     list_display = ('server_url', 'assoc_type')
     list_filter = ('assoc_type',)
+    search_fields = ('server_url',)
+    actions = ['cleanup_associations']
+
+    def cleanup_associations(self, request, queryset):
+        store = DjangoOpenIDStore()
+        count = store.cleanupAssociations()
+        self.message_user(request, "%d expired associations removed" % count)
+    cleanup_associations.short_description = "Clean up expired associations"
 
 admin.site.register(Association, AssociationAdmin)
 
+
 class UserOpenIDAdmin(admin.ModelAdmin):
-    list_display = ('user', 'claimed_id', 'display_id')
-    search_fields = ('user',)
+    list_display = ('user', 'claimed_id')
+    search_fields = ('claimed_id',)
 
 admin.site.register(UserOpenID, UserOpenIDAdmin)
