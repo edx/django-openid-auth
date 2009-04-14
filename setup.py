b6d3ef789767c7713148d802d91d0d3084fb82d1
Elliot Murphy <elliot@elliotmurphy.com>
2009-04-14 19:50:40 -0400

    Add long_description to setup.py as suggested in the review. Move debian packaging to a separate branch, as we'll want to make a release tarball that doesn't include the debian dir.

--- setup.py
+++ setup.py
@@ -33,6 +33,15 @@ from distutils.core import setup
 setup(name = 'django-openid-auth',
     version = '0.1',
     description = 'OpenID integration for django.contrib.auth',
+    long_description = """A library that can be used to add OpenID support to
+    Django applications. The library integrates with Django's built in
+    authentication system, so most applications require minimal changes to
+    support OpenID llogin. The library also includes the following features:
+    * Basic user details are transferred from the OpenID server via the simple
+      Registration extension.
+    * can be configured to use a fixed OpenID server URL, for use in SSO.
+    * supports the launchpad.net teams extension to get team membership info.
+    """,
     url = 'https://launchpad.net/django-openid-auth',
     packages = ['django_openid_auth',
                 'django_openid_auth/management',
