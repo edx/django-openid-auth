d7c3adfec6bff35189debb028ef7ef9d4814b54f
James Henstridge <james@jamesh.id.au>
2009-04-15 11:35:14 +0800

    Clean up the distutils code, and add a manifest to include the missing files.

--- setup.py
+++ setup.py
@@ -26,35 +26,49 @@
 # LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""setup.py"""
+"""OpenID integration for django.contrib.auth
+
+A library that can be used to add OpenID support to Django applications.
+The library integrates with Django's built in authentication system, so
+most applications require minimal changes to support OpenID llogin. The
+library also includes the following features:
+  * Basic user details are transferred from the OpenID server via the
+    simple Registration extension.
+  * can be configured to use a fixed OpenID server URL, for use in SSO.
+  * supports the launchpad.net teams extension to get team membership
+    info.
+"""
 
 from distutils.core import setup
 
-setup(name = 'django-openid-auth',
-    version = '0.1',
-    description = 'OpenID integration for django.contrib.auth',
-    long_description = """A library that can be used to add OpenID support to
-    Django applications. The library integrates with Django's built in
-    authentication system, so most applications require minimal changes to
-    support OpenID llogin. The library also includes the following features:
-    * Basic user details are transferred from the OpenID server via the simple
-      Registration extension.
-    * can be configured to use a fixed OpenID server URL, for use in SSO.
-    * supports the launchpad.net teams extension to get team membership info.
-    """,
-    url = 'https://launchpad.net/django-openid-auth',
-    packages = ['django_openid_auth',
-                'django_openid_auth/management',
-                'django_openid_auth/management/commands',
-                'django_openid_auth/templates',
-                'django_openid_auth/templates/openid',
-                'django_openid_auth/tests',
-                ],
-    classifiers = ['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Framework :: Django',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'])
+
+description, long_description = __doc__.split('\n\n', 1)
+
+setup(
+    name='django-openid-auth',
+    version='0.1',
+    author='Canonical Ltd',
+    description=description,
+    long_description=long_description,
+    license='BSD',
+    url='https://launchpad.net/django-openid-auth',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+        ],
+    packages=[
+        'django_openid_auth',
+        'django_openid_auth.management',
+        'django_openid_auth.management.commands',
+        'django_openid_auth.tests',
+        ],
+    package_data={
+        'django_openid_auth': ['templates/*.html', 'templates/openid/*.html'],
+        },
+    )
