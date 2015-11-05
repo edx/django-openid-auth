dbf4c9b7fcbb000ec21bb30ea39a69c1907bcb12
muhammad-ammar <mammar@gmail.com>
2015-11-05 15:10:35 +0500

    bump library version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ from setuptools import find_packages, setup
 
 
 description, long_description = __doc__.split('\n\n', 1)
-VERSION = '0.7'
+VERSION = '0.8'
 
 setup(
     name='django-openid-auth',
